9ca859143a2636abe66cc2ef47775f3bd930cfaf
miguelmoranv <118927132+miguelmoranv@users.noreply.github.com>
2023-01-13 14:37:25 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>MI PRACTICA CON CODESANDBOX</h1>
+<h2>MIGUEL ANGEL MORAN VAZQUEZ</h2>
+<div>
+UNIVERSIDAD TECNOLOGICA DE DURANGO 
+  <a href="https://github.com/miguelmoranv/practica1" target="_blank" rel="noopener noreferrer">MI GITHUB</a>.
+</div>
+`;
